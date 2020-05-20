e124961f2668ca62044c9ac06f48db55d58b6da4
Daniel Powell <danpowell88@gmail.com>
2020-05-20 21:57:55 +1000

    fix issue in logic causing all lights not to turn off

--- apps/Utilities/EntityActions.cs
+++ apps/Utilities/EntityActions.cs
@@ -1,26 +1,25 @@
-using System;
 using System.Linq;
 using System.Threading.Tasks;
-using EnumsNET;
 using JoySoftware.HomeAssistant.NetDaemon.Common;
 
 public static class EntityActions
 {
     public static async Task TurnEverythingOff(this NetDaemonApp app, string? roomName= null, params string[] excludeEntities)
     {
-        bool Area(IEntityProperties e) => 
-            roomName == null ? 
-                true :
-                (e.Attribute!.area != null && ((string)e.Attribute!.area).Split(",").Contains(roomName.ToLower()));
-
-        bool Entities(IEntityProperties e) =>
+        bool EntitiesToTurnOff(IEntityProperties e) =>
             (e.EntityId.StartsWith("light.") ||
              e.EntityId.StartsWith("fan.") ||
              e.EntityId.StartsWith("climate.")) &&
             !excludeEntities.ToList().Contains(e.EntityId);
+        
+        if (roomName == null)
+            await app.Entities(e => e.EntityId.StartsWith("light.")).TurnOff().ExecuteAsync();
+        else
+        {
+            bool Area(IEntityProperties e) => e.Attribute!.area != null && ((string)e.Attribute!.area).Split(",").Contains(roomName!.ToLower());
 
-        await app.Entities(e => Area(e) && Entities(e))
-            .TurnOff().ExecuteAsync();
+            await app.Entities(e => Area(e) && EntitiesToTurnOff(e)).TurnOff().ExecuteAsync();
+        }
 
         // TODO turn off switches not marked as always on
         // dont turn off bedroom climate if occupied
